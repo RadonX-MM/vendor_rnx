59e82f0d6d84b62659e01133595f7a04dd68de8c
Louis Popi <theh2o64@gmail.com>
2015-03-21 07:55:12 +0100

    aosparadox : Initial AOSParadox Vendor Repository
    * Based on CyanogenMod 12

---
+++ config/common.mk
@@ -0,0 +1,129 @@
+PRODUCT_BRAND ?= cyanogenmod
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+
+ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=android-google
+else
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
+endif
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    keyguard.no_require_sim=true \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.com.android.dateformat=MM-dd-yyyy \
+    ro.com.android.dataroaming=false
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.selinux=1
+
+# Thank you, please drive thru!
+PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
+
+ifneq ($(TARGET_BUILD_VARIANT),eng)
+# Enable ADB authentication
+ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
+endif
+
+# Backup Tool
+ifneq ($(WITH_GMS),true)
+PRODUCT_COPY_FILES += \
+    vendor/aosparadox/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/aosparadox/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/aosparadox/prebuilt/common/bin/50-cm.sh:system/addon.d/50-cm.sh \
+    vendor/aosparadox/prebuilt/common/bin/blacklist:system/addon.d/blacklist
+endif
+
+# init.d support
+PRODUCT_COPY_FILES += \
+    vendor/aosparadox/prebuilt/common/bin/sysinit:system/bin/sysinit
+
+# Enable SIP+VoIP on all targets
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+
+# Enable wireless Xbox 360 controller support
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:system/usr/keylayout/Vendor_045e_Product_0719.kl
+
+
+# Required packages
+PRODUCT_PACKAGES += \
+    Development \
+    LatinIME \
+    BluetoothExt
+
+# Optional packages
+PRODUCT_PACKAGES += \
+    VoicePlus \
+    Basic \
+    libemoji
+
+# Custom packages
+PRODUCT_PACKAGES += \
+    Launcher3
+
+# Extra tools
+PRODUCT_PACKAGES += \
+    libsepol \
+    e2fsck \
+    mke2fs \
+    tune2fs \
+    bash \
+    nano \
+    htop \
+    powertop \
+    lsof \
+    mount.exfat \
+    fsck.exfat \
+    mkfs.exfat \
+    mkfs.f2fs \
+    fsck.f2fs \
+    fibmap.f2fs \
+    ntfsfix \
+    ntfs-3g \
+    gdbserver \
+    micro_bench \
+    oprofiled \
+    sqlite3 \
+    strace
+
+# Openssh
+PRODUCT_PACKAGES += \
+    scp \
+    sftp \
+    ssh \
+    sshd \
+    sshd_config \
+    ssh-keygen \
+    start-ssh
+
+# rsync
+PRODUCT_PACKAGES += \
+    rsync
+
+# These packages are excluded from user builds
+ifneq ($(TARGET_BUILD_VARIANT),user)
+PRODUCT_PACKAGES += \
+    procmem \
+    procrank \
+    su
+endif
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.root_access=0
+
+PRODUCT_PACKAGE_OVERLAYS += vendor/aosparadox/overlay/common
+
+
+# by default, do not update the recovery with system updates
+PRODUCT_PROPERTY_OVERRIDES += persist.sys.recovery_update=false
+
+-include $(WORKSPACE)/build_env/image-auto-bits.mk
+
+$(call prepend-product-if-exists, vendor/extra/product.mk)
