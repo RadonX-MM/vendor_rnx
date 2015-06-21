57773be7336bdedd33382ab5f41fe7aff997dfa1
Louis Popi <theh2o64@gmail.com>
2015-06-21 15:25:11 +0200

    Delete local.rc and remove recovery bits in selinux

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -13,27 +13,18 @@ BOARD_SEPOLICY_UNION += \
     property_contexts \
     seapp_contexts \
     service_contexts \
-    adbd.te \
-    app.te \
     auditd.te \
-    bootanim.te \
-    drmserver.te \
     healthd.te \
     hostapd.te \
     installd.te \
-    mediaserver.te \
     netd.te \
     property.te \
-    recovery.te \
     shell.te \
-    su.te \
     sysinit.te \
     system.te \
     system_app.te \
-    system_server.te \
     ueventd.te \
     uncrypt.te \
     userinit.te \
     vold.te \
-    zygote.te \
     mac_permissions.xml
