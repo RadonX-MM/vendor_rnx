7bfd262520eeb328ecbab30e090701020cbcf1bd
Louis Popi <theh2o64@gmail.com>
2015-10-11 15:03:17 +0200

    [DO NOT MERGE] Disable sepolicy

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -1,30 +1,7 @@
 #
 # This policy configuration will be used by all products that
-# inherit from CM
+# inherit from AOSParadox
 #
 
-BOARD_SEPOLICY_DIRS += \
-    vendor/aosparadox/sepolicy
-
-BOARD_SEPOLICY_UNION += \
-    file.te \
-    file_contexts \
-    genfs_contexts \
-    property_contexts \
-    seapp_contexts \
-    service_contexts \
-    auditd.te \
-    healthd.te \
-    hostapd.te \
-    installd.te \
-    netd.te \
-    property.te \
-    shell.te \
-    sysinit.te \
-    system.te \
-    system_app.te \
-    ueventd.te \
-    uncrypt.te \
-    userinit.te \
-    vold.te \
-    mac_permissions.xml
+#BOARD_SEPOLICY_DIRS += \
+#    vendor/aosparadox/sepolicy
