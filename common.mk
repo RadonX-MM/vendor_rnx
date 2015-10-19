7cedb3ce27d1e77f66b31d85e201f3895abab220
Louis Popi <theh2o64@gmail.com>
2015-10-19 17:47:30 +0200

    vendor : Rework everything

--- common.mk
+++ common.mk
@@ -1,106 +1,84 @@
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+# Copyright (C) 2015 The AOSParadox Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
+# Properties
 PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=true \
-    ro.com.google.clientidbase=android-google \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.google.clientidbase=android-google \
     ro.com.android.wifi-watchlist=GoogleGuest \
     ro.setupwizard.enterprise_mode=1 \
     ro.com.android.dateformat=MM-dd-yyyy \
-    ro.com.android.dataroaming=false
+    ro.com.android.dataroaming=false \
+    net.tethering.noprovisioning=true \
+    persist.sys.dun.override=0 \
+    ro.build.selinux=1 \
+    ro.adb.secure=0 \
+    ro.error.receiver.system.apps=com.google.android.gms \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.setupwizard.network_required=false \
+    keyguard.no_require_sim=true \
+    ro.facelock.black_timeout=400 \
+    ro.facelock.det_timeout=1500 \
+    ro.facelock.rec_timeout=2500 \
+    ro.facelock.lively_timeout=2500 \
+    ro.facelock.est_max_time=600 \
+    ro.facelock.use_intro_anim=false \
+    ro.ril.enable.amr.wideband=1 \
+    drm.service.enabled=true \
+    ro.layers.noIcon=noIcon
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.build.selinux=1
+# Packages
+PRODUCT_PACKAGES += \
+    Launcher2 \
+    libemoji
 
-# Thank you, please drive thru!
-PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
-
-# Boot Animantion
-ifneq ($(filter falcon titan,$(TARGET_DEVICE)),)
-PRODUCT_COPY_FILES += \
-    vendor/aosparadox/prebuilt/common/media/AOSParadox_720_bootanimation.zip:system/media/bootanimation.zip
-endif
-ifneq ($(filter bacon,$(PRODUCT_DEVICE)),)
-PRODUCT_COPY_FILES += \
-    vendor/aosparadox/prebuilt/common/media/AOSParadox_1080_bootanimation.zip:system/media/bootanimation.zip
-endif
-ifneq ($(filter lux,$(PRODUCT_DEVICE)),)
-PRODUCT_COPY_FILES += \
-    vendor/aosparadox/prebuilt/common/media/AOSParadox_1080_bootanimation.zip:system/media/bootanimation.zip
-endif
-
-# init.d support
-PRODUCT_COPY_FILES += \
-    vendor/aosparadox/prebuilt/common/bin/sysinit:system/bin/sysinit \
-    vendor/aosparadox/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
-    vendor/aosparadox/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit
+# Common overlay
+PRODUCT_PACKAGE_OVERLAYS += \
+    vendor/aosparadox/overlay/common
 
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
-# Enable wireless Xbox 360 controller support
-PRODUCT_COPY_FILES += \
-    frameworks/base/data/keyboards/Vendor_045e_Product_028e.kl:system/usr/keylayout/Vendor_045e_Product_0719.kl
+# Media effects
+PRODUCT_COPY_FILES +=  \
+    vendor/aosparadox/prebuilt/common/media/LMspeed_508.emd:system/vendor/media/LMspeed_508.emd \
+    vendor/aosparadox/prebuilt/common/media/PFFprec_600.emd:system/vendor/media/PFFprec_600.emd
 
-# Misc packages
-PRODUCT_PACKAGES += \
-    BluetoothExt \
-    LatinIME \
-    Launcher3 \
-    libemoji \
-    Terminal
-
-# Stagefright FFMPEG plugin
-PRODUCT_PACKAGES += \
-    libstagefright_soft_ffmpegadec \
-    libstagefright_soft_ffmpegvdec \
-    libFFmpegExtractor \
-    libnamparser
-
-# Live Wallpapers and more Daydream packages
-PRODUCT_PACKAGES += \
-    Galaxy4 \
-    HoloSpiralWallpaper \
-    LiveWallpapers \
-    LiveWallpapersPicker \
-    MagicSmokeWallpapers \
-    NoiseField \
-    PhaseBeam \
-    VisualizationWallpapers \
-    PhotoTable \
-    SoundRecorder \
-    PhotoPhase
-
-# Telephony packages
-PRODUCT_PACKAGES += \
-    CellBroadcastReceiver \
-    Mms \
-    Stk
-
-# Mms depends on SoundRecorder for recorded audio messages
-PRODUCT_PACKAGES += \
-    SoundRecorder
-
-# World APN list
+# APN list
 PRODUCT_COPY_FILES += \
     vendor/aosparadox/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
-
-# Selective SPN list for operator number who has the problem.
+ 
+# Backup Tool
 PRODUCT_COPY_FILES += \
-    vendor/aosparadox/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+    vendor/aosparadox/prebuilt/common/addon.d/99-backup.sh:system/addon.d/99-backup.sh \
+    vendor/aosparadox/prebuilt/common/bin/backuptool.sh:system/bin/backuptool.sh \
+    vendor/aosparadox/prebuilt/common/bin/backuptool.functions:system/bin/backuptool.functions \
+    vendor/aosparadox/prebuilt/common/etc/backup.conf:system/etc/backup.conf
 
-# Overlays & Include LatinIME dictionaries
-PRODUCT_PACKAGE_OVERLAYS += \
-	vendor/aosparadox/overlay/common \
-	vendor/aosparadox/overlay/dictionaries
+# Boot Animantion
+ifneq ($(filter falcon titan,$(TARGET_DEVICE)),)
+PRODUCT_COPY_FILES += \
+    vendor/aosparadox/bootanimation/AOSParadox_720_bootanimation.zip:system/media/bootanimation.zip
+endif
+ifneq ($(filter bacon lux,$(PRODUCT_DEVICE)),)
+PRODUCT_COPY_FILES += \
+    vendor/aosparadox/bootanimation/AOSParadox_1080_bootanimation.zip:system/media/bootanimation.zip
+endif
 
 # AOSParadox Version
 PAR_VERSION := $(TARGET_DEVICE)-$(shell date -u +%Y%m%d)
 PRODUCT_PROPERTY_OVERRIDES += ro.par.version =$(PAR_VERSION)
-
-# by default, do not update the recovery with system updates
-PRODUCT_PROPERTY_OVERRIDES += persist.sys.recovery_update=false
-
-$(call inherit-product-if-exists, vendor/extra/product.mk)
--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -1,7 +1,6 @@
 #
 # This policy configuration will be used by all products that
-# inherit from AOSParadox
 #
 
-#BOARD_SEPOLICY_DIRS += \
-#    vendor/aosparadox/sepolicy
+BOARD_SEPOLICY_DIRS += \
+    vendor/aosparadox/sepolicy
