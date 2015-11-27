35e5cdf2bf555c3c8af4cde314ab25dc557dca0f
Joe Maples <frap129@gmail.com>
2015-11-26 22:53:54 -0500

    prebuilt: Add SuperSU
    Signed-off-by: Joe Maples <frap129@gmail.com>

---
+++ prebuilt/common/SuperSU/supersu.mk
@@ -0,0 +1,3 @@
+PRODUCT_COPY_FILES += \
+    vendor/rnx/prebuilt/common/SuperSU/SuperSU.zip:system/addon.d/SuperSU.zip \
+    vendor/rnx/prebuilt/common/SuperSU/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
