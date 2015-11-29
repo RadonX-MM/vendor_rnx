mintkat/vendor_benzo

vendor: Add Google DNS servers by way of resolv.conf

config/common.mk
@@ -53,6 +53,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/benzo/prebuilt/common/etc/init.benzo.rc:root/init.benzo.rc
 
+# Google DNS server
+PRODUCT_COPY_FILES += \
+    vendor/benzo/prebuilt/common/etc/resolv.conf:system/etc/resolv.conf
+
 # Enable SIP+VoIP
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml

RadonX-MM/vendor_rnx

Conflicts:
	config/common.mk

common.mk
@@ -53,6 +53,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGE_OVERLAYS += \
     vendor/rnx/overlay/common
 
+# Google DNS server
+PRODUCT_COPY_FILES += \
+    vendor/rnx/prebuilt/common/etc/resolv.conf:system/etc/resolv.conf
+
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
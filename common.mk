19a94b75dd6a9b35be5f4dd1ef6fc13bec532151
mydongistiny <jaysonedson@gmail.com>
2015-11-13 21:13:57 -0800

    vendor: Add Google DNS servers by way of resolv.conf
    Conflicts:
    	config/common.mk

--- common.mk
+++ common.mk
@@ -53,6 +53,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGE_OVERLAYS += \
     vendor/rnx/overlay/common
 
+# Google DNS server
+PRODUCT_COPY_FILES += \
+    vendor/rnx/prebuilt/common/etc/resolv.conf:system/etc/resolv.conf
+
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
